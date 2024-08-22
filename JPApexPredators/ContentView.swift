c3dd3933735214bdc3e9e02318582b1be07304c8
yunac <yunac.owob@gmail.com>
2024-08-22 23:44:52 +0800

    chore: init

---
+++ JPApexPredators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredators
+//
+//  Created by Yuna Chou on 2024/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Yuna Chou on 2024/8/22.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
