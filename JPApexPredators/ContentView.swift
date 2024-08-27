1717b428c4e92ac76095fd886432dd15e145ea48
yunac <yunac.owob@gmail.com>
2024-08-27 18:13:10 +0800

    feat(ui): implement dinosaur list view

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+import Inject
 
 struct ContentView: View {
+    @ObserveInjection var inject
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(DinosaurService().dinosaurs) { dinosaur in
+            HStack {
+                Image(dinosaur.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .white, radius: 1)
+
+
+                VStack(alignment: .leading) {
+                    Text(dinosaur.name)
+                        .fontWeight(.bold)
+
+                    Text(dinosaur.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 5)
+                        .background(dinosaur.type.background)
+                        .clipShape(.capsule)
+                }
+            }
         }
-        .padding()
+        .enableInjection()
+        .preferredColorScheme(.dark)
     }
 }
 
