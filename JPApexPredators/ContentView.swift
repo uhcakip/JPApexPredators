c6ea64c531d8836acf58886275affcfd6c47d6aa
yunac <yunac.owob@gmail.com>
2024-08-28 01:57:08 +0800

    feat(ui): implement NavigationView for dinosaur list

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -12,28 +12,37 @@ struct ContentView: View {
     @ObserveInjection var inject
 
     var body: some View {
-        List(DinosaurService().dinosaurs) { dinosaur in
-            HStack {
-                Image(dinosaur.imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
+        NavigationView {
+            List(DinosaurService().dinosaurs) { dinosaur in
+                NavigationLink {
+                    // TODO: Add a detail view
+                    Image(dinosaur.imageName)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        Image(dinosaur.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
 
+                        VStack(alignment: .leading) {
+                            Text(dinosaur.name)
+                                .fontWeight(.bold)
 
-                VStack(alignment: .leading) {
-                    Text(dinosaur.name)
-                        .fontWeight(.bold)
-
-                    Text(dinosaur.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(dinosaur.type.background)
-                        .clipShape(.capsule)
+                            Text(dinosaur.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(dinosaur.type.background)
+                                .clipShape(.capsule)
+                        }
+                    }
                 }
             }
+            .navigationTitle("Dinosaurs")
         }
         .enableInjection()
         .preferredColorScheme(.dark)
